3ab10ab08c53c5b4c541e85f14726c39c0fa2652
Milan <milance.za019@gmail.com>
2022-12-29 17:42:28 +0100

    Izrada pocetka sajta. Pocetna stranica. Naslov. Slika, kartica itd.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DoctorComponent
   ],
   imports: [
     BrowserModule,
